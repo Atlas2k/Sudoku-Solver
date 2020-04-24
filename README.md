27dc891d5f66e89513be94369aa234b5d9c69293
Atlas <amouri2000@hotmail.com>
2020-04-23 19:55:02 -0500

    Changes and additions to logic.py: Added all checks for rule compliance. Completed solveBoard func. Changed printBoard func. Added extra board to test.
    Edited README

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Sudoku-Solver
-Your average Sudoku solver project.  
-More info will be added as project progresses.
+This is a sudoku solver that uses backtracking to solve the board. It's basic I know, but hey I'm learning.  
+Note: The test boards were both copied from tutorials on the web. I promise I didn't cheat I just couldn't be asked to type out my own valid board.  
+A GUI will probably added in the future.
--- logic.py
+++ logic.py
@@ -4,26 +4,134 @@ def makeEmptyBoard():
 
 
 def printBoard(board):
-    for row in board:
-        print(row)
+    if type(board) == list:
+        for row in board:
+            print(row)
+    else:
+        print('This board is unsolvable.')
 
 
-def findEmptySpot(board, loc):
+def findEmptySpot(board):
+    loc = [0, 0]
     for row in range(9):
         for col in range(9):
             if board[row][col] == 0:
                 loc[0] = row
                 loc[1] = col
+                return loc, True
+    return loc, False
+
+
+def checkRow(board, loc, n):
+    if n in board[loc[0]]:
+        return False
+    else:
+        return True
+
+
+def checkCol(board, loc, n):
+    column = []
+    for c in board:
+        column.append(c[loc[1]])
+    if n in column:
+        return False
+    else:
+        return True
+
+
+def checkSquare(board, loc, n):
+    temp = []
+    squares = []
+    for y in range(0, 9, 3):
+        for x in range(0, 9):
+            temp += board[x][y:y+3]
+            if len(temp) == 9:
+                squares.append(temp)
+                temp = []
+    if 0 <= loc[1] <= 2:
+        if 0 <= loc[0] <= 2:
+            if n in squares[0]:
+                return False
+            else:
                 return True
+        elif 3 <= loc[0] <= 5:
+            if n in squares[1]:
+                return False
+            else:
+                return True
+        elif 6 <= loc[0] <= 8:
+            if n in squares[2]:
+                return False
+            else:
+                return True
+    elif 3 <= loc[1] <= 5:
+        if 0 <= loc[0] <= 2:
+            if n in squares[3]:
+                return False
+            else:
+                return True
+        elif 3 <= loc[0] <= 5:
+            if n in squares[4]:
+                return False
+            else:
+                return True
+        elif 6 <= loc[0] <= 8:
+            if n in squares[5]:
+                return False
+            else:
+                return True
+    elif 6 <= loc[1] <= 8:
+        if 0 <= loc[0] <= 2:
+            if n in squares[6]:
+                return False
+            else:
+                return True
+        elif 3 <= loc[0] <= 5:
+            if n in squares[7]:
+                return False
+            else:
+                return True
+        elif 6 <= loc[0] <= 8:
+            if n in squares[8]:
+                return False
+            else:
+                return True
+
+
+def solveBoard(board):
+    loc, Empty = findEmptySpot(board)
+    if not Empty:
+        return True
+    for n in range(1, 10):
+        if checkRow(board, loc, n)\
+                and checkCol(board, loc, n)\
+                and checkSquare(board, loc, n):
+            board[loc[0]][loc[1]] = n
+            if solveBoard(board):
+                return board
+            board[loc[0]][loc[1]] = 0
     return False
 
 
-testGrid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
-            [5, 2, 0, 0, 0, 0, 0, 0, 0],
-            [0, 8, 7, 0, 0, 0, 0, 3, 1],
-            [0, 0, 3, 0, 1, 0, 0, 8, 0],
-            [9, 0, 0, 8, 6, 3, 0, 0, 5],
-            [0, 5, 0, 0, 9, 0, 6, 0, 0],
-            [1, 3, 0, 0, 0, 0, 2, 5, 0],
-            [0, 0, 0, 0, 0, 0, 0, 7, 4],
-            [0, 0, 5, 2, 0, 6, 3, 0, 0]]
+board1 = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+          [5, 2, 0, 0, 0, 0, 0, 0, 0],
+          [0, 8, 7, 0, 0, 0, 0, 3, 1],
+          [0, 0, 3, 0, 1, 0, 0, 8, 0],
+          [9, 0, 0, 8, 6, 3, 0, 0, 5],
+          [0, 5, 0, 0, 9, 0, 6, 0, 0],
+          [1, 3, 0, 0, 0, 0, 2, 5, 0],
+          [0, 0, 0, 0, 0, 0, 0, 7, 4],
+          [0, 0, 5, 2, 0, 6, 3, 0, 0]]
+
+board2 = [[7, 8, 0, 4, 0, 0, 1, 2, 0],
+          [6, 0, 0, 0, 7, 5, 0, 0, 9],
+          [0, 0, 0, 6, 0, 1, 0, 7, 8],
+          [0, 0, 7, 0, 4, 0, 2, 6, 0],
+          [0, 0, 1, 0, 5, 0, 9, 3, 0],
+          [9, 0, 4, 0, 6, 0, 0, 0, 5],
+          [0, 7, 0, 3, 0, 0, 0, 1, 2],
+          [1, 2, 0, 0, 0, 7, 4, 0, 0],
+          [0, 4, 9, 2, 0, 6, 0, 0, 7]]
+
+
+printBoard(solveBoard(board1))
